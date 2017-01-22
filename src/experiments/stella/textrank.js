e32193508bbd1711bfa6c45da77bdc524879b342
dantetam <dante.tam1@gmail.com>
2017-01-22 11:42:37 -0800

    TextRank algorithm

--- src/experiments/stella/textrank.js
+++ src/experiments/stella/textrank.js
@@ -13,16 +13,50 @@ function createGraphFromSentences(listSentences) {
   var graph = [];
   for (var i = 0; i < listSentences.length(); i++) {
     var node = {
+      importance: 1.0,
       sentence: listSentences[i],
-      outVertices = {},
-      inVertices = {}
+      neighbors = {}
     }
     for (var j = 0; j < listSentences.length(); j++) {
       if (i === j) continue;
+      var sentenceSimilarity = sentenceSimilarity(listSentences[i], listSentences[j]);
+      node.neighbors[j] = sentenceSimilarity;
     }
     graph.push(node);
   }
+  return graph;
 }
+
+function iterateTextrankGraph(graph, d=0.85) {
+  var newImportances = [];
+  for (var i = 0; i < graph.length(); i++) {
+    var nodeI = graph[i];
+
+    var sum = 0;
+    var nodeIAdj = Object.keys(nodeI.neighbors);
+    for (var j = 0; j < nodeIAdj.length; j++) {
+      var nodeJ = graph[nodeIAdj[j]];
+      var weightIJ = nodeI.neighbors[nodeIAdj[j]];
+
+      var nodeJAdj = Object.keys(nodeJ.neighbors);
+      var jkSum = 0;
+      for (var k = 0; k < nodeJAdj.length; k++) {
+        var weightJK = nodeJ.neighbors[nodeJAdj[k]];
+        jkSum += weightJK;
+      }
+
+      sum += weightIJ * nodeJ.importance / jkSum;
+    }
+
+    var newScore = (1-d) + d*sum;
+    newImportances.push(newScore);
+  }
+  for (var i = 0; i < graph.length(); i++) {
+    graph[i].importance = newImportances[i];
+  }
+  return graph;
+}
+
 
 
 
