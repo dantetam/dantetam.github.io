7d7d83c7e821f61c3b575d586764e07abc74a202
dantetam <dante.tam1@gmail.com>
2017-02-02 11:42:50 -0800

    Test GCSE input events

---
+++ src/experiments/stella/searchweb.js
@@ -0,0 +1,11 @@
+function lookGcseResults() {
+  var elems = document.getElementsByClassName("gsc-webResult gsc-result");
+  for (var i = 0; i < elems.length; i++) {
+    var elem = elems[i];
+    console.log(elem);
+  }
+}
+
+function testFunc() {
+  console.log("hi");
+}
