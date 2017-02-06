a279cb5c193a1c61cec999d40399fc08d716dd5f
dantetam <dante.tam1@gmail.com>
2017-02-05 17:42:45 -0800

    Parse data from GCSE web search results

--- src/experiments/stella/searchweb.js
+++ src/experiments/stella/searchweb.js
@@ -68,21 +68,70 @@ String.prototype.replaceAll = function(str1, str2, ignore)
 
 //Only use with trusted sources. This could potentially execute arbitrary JS code.
 function getTextFromHtml(html) {
+  var result = "";
   var tmp = document.createElement("DIV");
   tmp.innerHTML = html;
   $(tmp).find("p").each(function() {
-    console.log(this);
+    result += this.textContent || this.innerText || "";
   });
-  return tmp.textContent || tmp.innerText || "";
+  return result;
 }
 
 var body = document.getElementsByTagName('body')[0];
-var x2js = new X2JS();
+
+/*	This work is licensed under Creative Commons GNU LGPL License.
+
+	License: http://creativecommons.org/licenses/LGPL/2.1/
+   Version: 0.9
+	Author:  Stefan Goessner/2006
+	Web:     http://goessner.net/
+*/
+function json2xml(o, tab="") {
+   var toXml = function(v, name, ind) {
+      var xml = "";
+      if (v instanceof Array) {
+         for (var i=0, n=v.length; i<n; i++)
+            xml += ind + toXml(v[i], name, ind+"\t") + "\n";
+      }
+      else if (typeof(v) == "object") {
+         var hasChild = false;
+         xml += ind + "<" + name;
+         for (var m in v) {
+            if (m.charAt(0) == "@")
+               xml += " " + m.substr(1) + "=\"" + v[m].toString() + "\"";
+            else
+               hasChild = true;
+         }
+         xml += hasChild ? ">" : "/>";
+         if (hasChild) {
+            for (var m in v) {
+               if (m == "#text")
+                  xml += v[m];
+               else if (m == "#cdata")
+                  xml += "<![CDATA[" + v[m] + "]]>";
+               else if (m.charAt(0) != "@")
+                  xml += toXml(v[m], m, ind+"\t");
+            }
+            xml += (xml.charAt(xml.length-1)=="\n"?ind:"") + "</" + name + ">";
+         }
+      }
+      else {
+         xml += ind + "<" + name + ">" + v.toString() +  "</" + name + ">";
+      }
+      return xml;
+   }, xml="";
+   for (var m in o)
+      xml += toXml(o[m], m, "");
+   return tab ? xml.replace(/\t/g, tab) : xml.replace(/\t|\n/g, "");
+}
+
 
 function linkAnalyzeCallback(data) {
-  var xml = x2js.json2xml_str(data);
-  var text = getTextFromHtml(xml).replaceAll('&quot;', '"');
-  console.log(text);
+  console.log(data);
+  var xml = json2xml(data);
+  console.log(xml);
+  var temp = getTextFromHtml(xml);
+  console.log(temp);
 }
 
 function lookGcseResults() {
@@ -103,7 +152,7 @@ function lookGcseResults() {
 
           }
           script.src = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22' + link + '%22&format=json&diagnostics=false&callback=linkAnalyzeCallback';
-          console.log('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22' + link + '%22&format=json&diagnostics=false&callback=linkAnalyzeCallback');
+          //console.log('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22' + link + '%22&format=json&diagnostics=false&callback=linkAnalyzeCallback');
           document.getElementsByTagName('body')[0].appendChild(script);
         }
       }
