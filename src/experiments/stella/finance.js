e66f23422df250a1278e1e98256e718275549fde
dantetam <dante.tam1@gmail.com>
2017-02-17 00:29:17 -0800

    Attempt to get EPS data from NASDAQ API

--- src/experiments/stella/finance.js
+++ src/experiments/stella/finance.js
@@ -149,6 +149,37 @@ function getDataForSymbols(listOfStockSymbols) {
   body.appendChild(script);
 }
 
+function getTablesFromHTML(html) {
+  var result = "";
+  var tmp = document.createElement("DIV");
+  tmp.innerHTML = html;
+  $(tmp).find("table").each(function() {
+    console.log(this);
+  });
+  return result;
+}
+
+function analyzeEPS(data) {
+  var xml = json2xml(data);
+  var tables = getTablesFromHTML(xml);
+}
+
+function getEPSData(listOfStockSymbols) {
+  for (var i = 0; i < listOfStockSymbols.length; i++) {
+    var symbol = listOfStockSymbols[i];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.onload = function() {
+
+    }
+    //script.src = 'https://chartapi.finance.yahoo.com/instrument/1.0/' + tickerString + '/chartdata;type=quote;range=1d/json/?callback=financeAnalyze';
+    //script.src = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20pm.finance.graphs%20where%20symbol%20in%20(%22YHOO%22%2C%22AAPL%22%2C%22GOOG%22%2C%22MSFT%22)&diagnostics=true&callback=finance';
+    //script.src = 'https://autoc.finance.yahoo.com/autoc?query=' + tickerString +
+    script.src = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22http://www.nasdaq.com/symbol/' + symbol + '/revenue-eps%22&format=json&diagnostics=false&callback=analyzeEPS';
+    body.appendChild(script);
+  }
+}
+
 //Only use with trusted sources. This could potentially execute arbitrary JS code.
 /*
 function getTextFromHtml(html) {
