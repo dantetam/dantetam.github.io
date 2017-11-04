58b92831fef377a69655b8d5f929f2e5851ead74
dantetam <dante.tam1@gmail.com>
2017-11-04 01:58:14 -0700

    Small changes to website

--- src/experiments/info190/midterm/main.js
+++ src/experiments/info190/midterm/main.js
@@ -36,131 +36,146 @@ var h1 = d3.select("body").append("h1");
 var mainGroup = g.append("g")
   .attr("class", "main-group");
 
-function loadData(varNameX, varNameY, varNameColor) {
+d3.csv(dataPath, renderCallback);  
+  
+function loadData(data, varNameX, varNameY, varNameColor) {
   h1.text("Testing scatterplot: x = " + varNameX + ", y = " + varNameY + ", color = " + varNameColor);
-  d3.csv(dataPath, function(err, data) {
-    if (err) {
-      throw err;
+  
+  var uniqueYears = data.reduce(function(acc, cur) {
+    if (acc.indexOf(cur[varNameColor]) == -1) {
+      acc.push(cur[varNameColor]);
     }
-    console.log(data);
+    return acc;
+  }, []);
+  //Possibly sort uniqueYears in some way?
+  
+  var colorScale = function(t) {
+    //The two lines work only when interpolating numbered data
+    //var lastYear = +uniqueYears[uniqueYears.length - 1];
+    //var percent = (+t - +uniqueYears[0]) / (lastYear - +uniqueYears[0]);
     
-    var uniqueYears = data.reduce(function(acc, cur) {
-      if (acc.indexOf(cur[varNameColor]) == -1) {
-        acc.push(cur[varNameColor]);
-      }
-      return acc;
-    }, []);
+    //Below we extend it to look at categorical data
+    var percent = uniqueYears.indexOf(t) / uniqueYears.length;
+    return d3.interpolateBlues(percent);
+  }; 
+  
+  //colorScale.range(d3.schemeCategory20).domain(uniqueYears);
     
-    console.log(uniqueYears);
+  xScale.domain(d3.extent(data, function(d) { //Return the min/max value of a dataset, accessed
+    return +d[varNameX]; //Get all the dates of every data object
+  }));  
     
-    //Possibly sort uniqueYears in some way?
+  yScale.domain(d3.extent(data, function(d) {
+    return +d[varNameY];
+  }));
+  
+  //Remove the old axes, if they exist
+  g.select(".y-axis").remove();
+  g.select(".x-axis").remove();
+  
+  //Create new group elements for the axes and append them to our graph
+  g.append("g")
+    .attr("class", "y-axis")
+    .call(yAxis);
+  g.append("g")
+    .attr("class", "x-axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
     
-    var colorScale = function(t) {
-      //The two lines work only when interpolating numbered data
-      //var lastYear = +uniqueYears[uniqueYears.length - 1];
-      //var percent = (+t - +uniqueYears[0]) / (lastYear - +uniqueYears[0]);
-      
-      //Below we extend it to look at categorical data
-      var percent = uniqueYears.indexOf(t) / uniqueYears.length;
-      console.log(t);
-      return d3.interpolateBlues(percent);
-    }; 
-    
-    //colorScale.range(d3.schemeCategory20).domain(uniqueYears);
-      
-    xScale.domain(d3.extent(data, function(d) { //Return the min/max value of a dataset, accessed
-      return +d[varNameX]; //Get all the dates of every data object
-    }));  
-      
-    yScale.domain(d3.extent(data, function(d) {
-      return +d[varNameY];
-    }));
-    
-    //Remove the old axes, if they exist
-    g.select(".y-axis").remove();
-    g.select(".x-axis").remove();
-    
-    //Create new group elements for the axes and append them to our graph
-    g.append("g")
-      .attr("class", "y-axis")
-      .call(yAxis);
-    g.append("g")
-      .attr("class", "x-axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
-      
-    var t = g.transition().duration(750); //Create a new transition object
-    
-    mainGroup.datum(data); //Pass in a singular data value, i.e. the site's data values
-   
-    //The general update pattern, order does not matter since it's all
-    //processed in one frame
+  var t = g.transition().duration(750); //Create a new transition object
+  
+  mainGroup.datum(data); //Pass in a singular data value, i.e. the site's data values
+ 
+  //The general update pattern, order does not matter since it's all
+  //processed in one frame
 
-    var circles = mainGroup.selectAll("circle") //Nested selection/append
-      .data(function(d) {return d;});
+  var circles = mainGroup.selectAll("circle") //Nested selection/append
+    .data(function(d) {return d;});
+  
+  //Remove data not being shown, in the exit()
+  circles.exit()
+    .transition(t)
+    .attr('r', 0)
+    .style('fill', "rgba(255, 255, 255, 0)")
+    .remove();
+  
+  
+  //Each of these circles is already offset
+  //Update circles that already exist
+  circles
+    .transition(t)
+    .attr("cy", function(d) {
+      return yScale(+d[varNameY]);
+    })
+    .attr("cx", function(d) {
+      return xScale(+d[varNameX]);
+    })
+    .attr("r", function(d) {
+      return 3;
+    })
+    .attr("fill", function(d) {
+      //return colorScale(d["year"]);
+      return colorScale(d[varNameColor]);
+    })
+    .attr("stroke", function(d) {
+      //return colorScale(d["year"]);
+      return "black";
+    });
     
-    //Remove things in the exit
-    circles.exit()
-      .transition(t)
-      .attr('r', 0)
-      .style('fill', "rgba(255, 255, 255, 0)")
-      .remove();
-    
-    
-    //Each of these circles is already offset
-    //Update circles that already exist
-    circles
-      .transition(t)
-      .attr("cy", function(d) {
-        return yScale(+d[varNameY]);
-      })
-      .attr("cx", function(d) {
-        return xScale(+d[varNameX]);
-      })
-      .attr("r", function(d) {
-        return 3;
-      })
-      .attr("fill", function(d) {
-        //return colorScale(d["year"]);
-        return colorScale(d[varNameColor]);
-      });
-      
-    //Add/append new circles when there are more than exist
-    circles.enter().append("circle")
-      .transition(t)
-      .attr("cy", function(d) {
-        return yScale(+d[varNameY]);
-      })
-      .attr("cx", function(d) {
-        return xScale(+d[varNameX]);
-      })
-      .attr("r", function(d) {
-        return 3;
-      })
-      .attr("fill", function(d) {
-        //return colorScale(d["year"]);
-        return colorScale(d[varNameColor]);
-      });
-  });
-}
-
-loadData("cylinders", "horsepower", "year");
-
-var dataTypes = ["acceleration", "cylinders", "displacement", "horsepower", "mpg", "weight"];
-var allVarCombinations = [];
-
-for (var i = 0; i < dataTypes.length; i++) {
-  for (var j = 0; j < dataTypes.length; j++) {
-    if (i === j) continue;
-    allVarCombinations.push([dataTypes[i], dataTypes[j]]);
+  //Add/append new circles when there are more than exist
+  circles.enter().append("circle")
+    .transition(t)
+    .attr("cy", function(d) {
+      return yScale(+d[varNameY]);
+    })
+    .attr("cx", function(d) {
+      return xScale(+d[varNameX]);
+    })
+    .attr("r", function(d) {
+      return 3;
+    })
+    .attr("fill", function(d) {
+      //return colorScale(d["year"]);
+      return colorScale(d[varNameColor]);
+    })
+    .attr("stroke", function(d) {
+      //return colorScale(d["year"]);
+      return "black";
+    });
+}  
+  
+//This architecture is to only load all of the data once, and then access it accordingly
+function renderCallback(err, data) {
+  if (err) {
+    throw err;
   }
+  console.log(data);
+  
+  //Cycle through all possible combinations of x and y variables
+ 
+  var dataTypes = ["acceleration", "cylinders", "displacement", "horsepower", "mpg", "weight"];
+  var allVarCombinations = [];
+
+    
+  for (var i = 0; i < dataTypes.length; i++) {
+    for (var j = 0; j < dataTypes.length; j++) {
+      if (i === j) continue;
+      allVarCombinations.push([dataTypes[i], dataTypes[j]]);
+    }
+  }
+  
+  //This callback has access to all the data, within scope
+  allVarCombinations.forEach(function(item, i) {
+    d3.timeout(function() {
+      loadData(data, item[0], item[1], "year")
+    }, 3000 * i);
+  });
+  
+  //loadData(data, "acceleration", "cylinders", "year")
+
 }
 
-allVarCombinations.forEach(function(item, i) {
-  d3.timeout(function() {
-    loadData(item[0], item[1], "year")
-  }, 3000 * (i + 1));
-});
+//loadData("cylinders", "horsepower", "year");
 
   
   
